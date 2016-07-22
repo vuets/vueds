93c2418d5eb22ed4cf891ad3a522a983d51d24f0
dyu <david.yu.ftw@gmail.com>
2016-07-22 17:02:16 +0800

    diff, merge

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,6 +5,15 @@ export declare namespace MultiCAS {
     function $stringify(obj: any): string;
     const $descriptor: {};
 }
+export interface KV {
+    k: string;
+    v: any;
+}
 export declare function escapeValue(v: string): string;
 export declare function mergeVmFrom<T>(src: any, descriptor: any, target: T): T;
 export declare function mergePojoFrom<T>(src: any, descriptor: any, target: T): T;
+export declare function writeKVsTo<T>(target: T, kvs: KV[]): T;
+export declare function diffFieldTo<T>(mc: MultiCAS, descriptor: any, original: T, modified: T, field: number): boolean;
+export declare function diffTo<T>(mc: MultiCAS, descriptor: any, original: T, modified: T): KV[];
+export declare function diffVmFieldTo<T>(mc: MultiCAS, descriptor: any, original: T, modified: T, field: string | number): boolean;
+export declare function diffVmTo<T>(mc: MultiCAS, descriptor: any, original: T, modified: T): KV[];
--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,13 @@ export namespace MultiCAS {
         return ''
     }
     export const $descriptor = {
-    
+
     }
+}
+
+export interface KV {
+    k: string
+    v: any
 }
 
 export function escapeValue(v: string): string {
@@ -25,7 +30,7 @@ export function mergeVmFrom<T>(src: any, descriptor: any, target: T): T {
     for (var i in src) {
         if ((fd = descriptor[i])) target[fd.$] = src[i]
     }
-    
+
     return target
 }
 
@@ -37,5 +42,102 @@ export function mergePojoFrom<T>(src: any, descriptor: any, target: T): T {
     }
 
     return target
+}
+
+export function writeKVsTo<T>(target: T, kvs: KV[]): T {
+    for (let kv of kvs) {
+        target[kv.k] = kv.v
+    }
+
+    return target
+}
+
+// must be a scalar field
+export function diffFieldTo<T>(mc: MultiCAS, descriptor: any, original: T, modified: T,
+        field: number): boolean {
+    var d, t, k, array, forig, fmod,
+        i = String(field)
+
+    if (!i ||
+        !(d = descriptor[i]) ||
+        !d.m ||
+        !(t = d.t) ||
+        (fmod = modified[i]) === (forig = original[i])) return false
+
+    // enum type converted to int
+    k = t === 16 ? '8' : String(t)
+    if (!(array = mc[k]))
+        mc[k] = array = []
+
+    array.push({ '1': parseInt(i, 10), '2': forig, '3': fmod })
+
+    return true
+}
+
+// only the scalar fields are diffed
+export function diffTo<T>(mc: MultiCAS, descriptor: any, original: T, modified: T): KV[] {
+    var d, t, k, array, forig, fmod, diffed: KV[] = []
+
+    for (var i in modified) {
+        if (!(d = descriptor[i]) ||
+            !d.m ||
+            !(t = d.t) ||
+            (fmod = modified[i]) === (forig = original[i])) continue
+
+        // enum type converted to int
+        k = t === 16 ? '8' : String(t)
+        if (!(array = mc[k]))
+            mc[k] = array = []
+
+        array.push({ '1': parseInt(i, 10), '2': forig, '3': fmod })
+        diffed.push({ k: i, v: fmod })
+    }
+
+    return diffed
+}
+
+// must be a scalar field
+export function diffVmFieldTo<T>(mc: MultiCAS, descriptor: any, original: T, modified: T,
+        field: string | number): boolean {
+    var d, t, k, array, forig, fmod,
+        i = isNaN(field as any) ? descriptor.$[field] : String(field)
+
+    if (!i ||
+        !(d = descriptor[i]) ||
+        !d.m ||
+        !(t = d.t) ||
+        (fmod = modified[d.$]) === (forig = original[i])) return false
+
+    // enum type converted to int
+    k = t === 16 ? '8' : String(t)
+    if (!(array = mc[k]))
+        mc[k] = array = []
+
+    array.push({ '1': parseInt(i, 10), '2': forig, '3': fmod })
+
+    return true
+}
+
+// only the scalar fields are diffed
+export function diffVmTo<T>(mc: MultiCAS, descriptor: any, original: T, modified: T): KV[] {
+    var d, t, k, array, forig, fmod, i, diffed: KV[] = []
+
+    for (var vi in modified) {
+        if (!(i = descriptor.$[vi]) ||
+            !(d = descriptor[i]) ||
+            !d.m ||
+            !(t = d.t) ||
+            (fmod = modified[vi]) === (forig = original[i])) continue
+
+        // enum type converted to int
+        k = t === 16 ? '8' : String(t)
+        if (!(array = mc[k]))
+            mc[k] = array = []
+
+        array.push({ '1': parseInt(i, 10), '2': forig, '3': fmod })
+        diffed.push({ k: i, v: fmod })
+    }
+
+    return diffed
 }
 
