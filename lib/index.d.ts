14ec539dc78166457635fa41ec0844613bb99867
dyu <david.yu.ftw@gmail.com>
2016-10-01 23:26:44 +0800

    generated files

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -61,4 +61,4 @@ export declare function verifyFormFields(message: any, descriptor: any, root?: a
 /**
  * The update arg means if existing data is modified (not creating new data).
  */
-export declare function $change(event: any, message: any, field: string | number, update: boolean): string | null;
+export declare function $change(e: any, message: any, field: string | number, update: boolean, root: any): string | null;
