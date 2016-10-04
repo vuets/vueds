60e09ac2cb7f6fe33b1ded30674f1fe287892b51
dyu <david.yu.ftw@gmail.com>
2016-10-04 18:54:04 +0800

    generated files

--- lib/datetime_util.d.ts
+++ lib/datetime_util.d.ts
@@ -1,6 +1,6 @@
 export declare function newTimeFormatArray(one: any, ago: any, fromNow: any, aSecondAgo: any, justNow: any, seconds: any, minute: any, minutes: any, hour: any, hours: any, yesterday: any, tomorrow: any, days: any, lastWeek: any, nextWeek: any, weeks: any, lastMonth: any, nextMonth: any, months: any, lastYear: any, nextYear: any, years: any, lastCentury: any, nextCentury: any, centuries: any): any[];
 export declare const defaultTimeFormatArray: any[];
-export declare function prettyDate(ts: number, targetTimeFormatArray: string[], ago?: string, fromNow?: string): string;
+export declare function prettyDate(ts: number, targetTimeFormatArray?: string[], ago?: string, fromNow?: string): string;
 export declare function formatDate2(date: Date, fmt: string): string;
 export declare function toDay(date: Date): string;
 export declare function toD(date: Date): string;
