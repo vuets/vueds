8cfed33e6ef7720d1a966863ddb7e5d78ac322be
dyu <david.yu.ftw@gmail.com>
2016-10-28 17:42:29 +0800

    generated files

--- lib/rpc/index.d.ts
+++ lib/rpc/index.d.ts
@@ -1,3 +1,5 @@
-export declare function handler<T>(res: any): T;
-export declare function post<T>(url: string, data: string, config?: any): PromiseLike<T>;
-export declare function get<T>(url: string): PromiseLike<T>;
+import 'whatwg-fetch';
+export declare function checkStatus<T>(res: any): T;
+export declare function handler<T>(raw: string): T;
+export declare function post<T>(url: string, data: string, customHandler?: any): PromiseLike<T>;
+export declare function get<T>(url: string, customHandler?: any): PromiseLike<T>;
