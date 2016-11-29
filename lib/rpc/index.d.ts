13184320c0bfcf1940e5b6e9d8d470d1f833c891
dyu <david.yu.ftw@gmail.com>
2016-11-30 01:32:25 +0800

    generated files

--- lib/rpc/index.d.ts
+++ lib/rpc/index.d.ts
@@ -2,7 +2,10 @@ import 'whatwg-fetch';
 export interface HasToken {
     token: string;
 }
+export declare type AuthOk = (token: string) => void;
+export declare type AuthHandler = (cb: AuthOk) => void;
 export declare function checkStatus<T>(res: any): T;
 export declare function handler<T>(raw: string): T;
+export declare function setAuthHandler(handler: AuthHandler): void;
 export declare function post<T>(url: string, data: string, customHandler?: any): PromiseLike<T>;
 export declare function get<T>(url: string, opts?: any, customHandler?: any): PromiseLike<T>;
