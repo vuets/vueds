0e6f05512e525b3700f829f70d77bd1da600e7b3
dyu <david.yu.ftw@gmail.com>
2017-01-04 08:17:06 +0800

    generated files

--- lib/rpc/index.d.ts
+++ lib/rpc/index.d.ts
@@ -4,8 +4,29 @@ export interface HasToken {
 }
 export declare type AuthOk = (token: string) => void;
 export declare type AuthHandler = (cb: AuthOk) => void;
+export declare type GetHandler = <T>(location: string, opts?: any) => PromiseLike<T>;
+export declare type PostHandler = <T>(location: string, data: string) => PromiseLike<T>;
 export declare function checkStatus<T>(res: any): T;
 export declare function handler<T>(raw: string): T;
+export interface Config {
+    auth$$?: AuthHandler;
+    get$$: GetHandler;
+    post$$: PostHandler;
+}
 export declare function setAuthHandler(handler: AuthHandler): void;
-export declare function post<T>(url: string, data: string, customHandler?: any): PromiseLike<T>;
-export declare function get<T>(url: string, opts?: any, customHandler?: any): PromiseLike<T>;
+export declare function post<T>(location: string, data: string): PromiseLike<T>;
+export declare function get<T>(location: string, opts?: any): PromiseLike<T>;
+export declare class P {
+    private ah;
+    private url;
+    private opts;
+    private h;
+    handlers: any[];
+    cbFail: any;
+    authOk: AuthOk | null;
+    constructor(ah: AuthHandler | null, url: string, opts: any, h: any);
+    then(resolve: any, reject: any): P;
+    catch(reject: any): P;
+    run(token?: string): void;
+    fail(err: any): void;
+}
